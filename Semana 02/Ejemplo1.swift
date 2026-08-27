f565a04ff9f2baf6a1909ef30fec46d66de861c5
Gabriel <gabriel.haro@tecsup.edu.pe>
2026-08-27 12:14:48 -0500

    Ingreso de los datos al código

---
+++ Semana 02/Ejemplo1.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+let parcial = 0.3
+let trabajo = 0.3
+let final = 0.4
+
+print("Nombre del alumno ")
+let alumno = readLine()!
+
+print("Nota parcial ")
+let parcialNota = Double(readLine() ?? ) ?? 0
+// commit del ingreso de datos
+// commit del calculo
+// commit de mostrar los datos
---
+++ Semana 02/Laboratorio2.swift
@@ -0,0 +1,8 @@
+import Foundation
+print ("Numero 1")
+let a = Double(readLine() ?? "") ?? 0
+print ("Numero 2")
+let b = Double(readLine() ?? "") ?? 0
+
+let suma = a + b
+print ("\(suma)")
