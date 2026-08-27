3ddfbd3385ebd0f1ab038ea5b59270630927f319
Gabriel <gabriel.haro@tecsup.edu.pe>
2026-08-27 13:17:01 -0500

    Realizando el calculo con sus multas progresivas.

---
+++ Semana 02/Caso01.swift
@@ -0,0 +1,46 @@
+import Foundation
+let dateFormatter = DateFormatter()
+dateFormatter.dateFormat = "dd/MM/yyyy"
+
+//Primer commit: Ingreso de datos.
+print("Título del libro:")
+let tituloLibro = readLine() ?? ""
+
+print("Tipo de Usuario (Alumno / Docente / Administrador):")
+let tipoUsuario = (readLine() ?? "").capitalized
+
+print("Fecha de Préstamo (dd/MM/yyyy):")
+let fechaPrestamoStr = readLine() ?? ""
+
+print("Fecha de Devolución Real (dd/MM/yyyy):")
+let fechaDevolucionStr = readLine() ?? ""
+
+//Segundo Commit: El cálculo con su multa progresiva.
+var diasLimite = 7
+var multaBaseDiaria = 1.5
+
+if tipoUsuario == "Docente"{
+    diasLimite = 15
+    multaBaseDiaria = 2.0
+} else if tipoUsuario == "Administrador"{
+    diasLimite = 10
+    multaBaseDiaria = 3.0
+}
+
+let fPrestamo = dateFormatter.date(from: fechaPrestamoStr) ?? Date()
+let fDevolucion = dateFormatter.date(from: fechaDevolucionStr) ?? Date()
+
+let componentes = Calendar.current.dateComponents([.day], from: fPrestamo, to: fDevolucion)
+let diasExedidos = componentes.day ?? 0
+let diasAtraso = max(0, diasExedidos - diasLimite)
+
+var multaAcumulada = 0.0
+for dia in 1...diasAtraso{
+    var costoDia = multaBaseDiaria
+    if dia >= 4 && dia <= 6{
+        costoDia *= 1.5
+    } else if dia > 7{
+        costoDia *= 2.0
+    }
+    multaAcumulada += costoDia
+}
