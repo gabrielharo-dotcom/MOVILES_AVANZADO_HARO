8e140ff583eac6cf9e46549dca0b27d5ecdb299e
gabrielharo-dotcom <gabriel.haro@tecsup.edu.pe>
2026-09-14 19:20:00 -0500

    feat: Actividad 02 - heredar clientes naturales y juridicos

--- Semana 04/Actividad02.playground/Contents.swift
+++ Semana 04/Actividad02.playground/Contents.swift
@@ -27,3 +27,49 @@ class Cliente {
     }
 }
 
+class ClienteNatural: Cliente {
+    let nombreCompleto: String
+    let dni: String
+
+    init(nombreCompleto: String, dni: String, codigo: String,
+         direccion: String, fechaDeRegistro: String,
+         numeroCuenta: String, montoMinimoApertura: Double) {
+        self.nombreCompleto = nombreCompleto
+        self.dni = dni
+        // La clase base inicializa los datos que ambos tipos comparten.
+        super.init(codigo: codigo, direccion: direccion, fechaDeRegistro: fechaDeRegistro,
+                   numeroCuenta: numeroCuenta, montoMinimoApertura: montoMinimoApertura)
+    }
+
+    override func mostrarDatos() {
+        print("👤 Cliente Natural:")
+        print("Nombre: \(nombreCompleto)")
+        print("DNI: \(dni)")
+        super.mostrarDatos()
+    }
+}
+
+class ClienteJuridico: Cliente {
+    let razonSocial: String
+    let ruc: String
+    let representanteLegal: String
+
+    init(razonSocial: String, ruc: String, representanteLegal: String,
+         codigo: String, direccion: String, fechaDeRegistro: String,
+         numeroCuenta: String, montoMinimoApertura: Double) {
+        self.razonSocial = razonSocial
+        self.ruc = ruc
+        self.representanteLegal = representanteLegal
+        super.init(codigo: codigo, direccion: direccion, fechaDeRegistro: fechaDeRegistro,
+                   numeroCuenta: numeroCuenta, montoMinimoApertura: montoMinimoApertura)
+    }
+
+    override func mostrarDatos() {
+        print("🏢 Cliente Jurídico:")
+        print("Razón Social: \(razonSocial)")
+        print("RUC: \(ruc)")
+        print("Representante Legal: \(representanteLegal)")
+        super.mostrarDatos()
+    }
+}
+
